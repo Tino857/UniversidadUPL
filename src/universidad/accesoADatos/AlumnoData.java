aabe58418b5ab5dd9d2f8fc02de633f87ad3b319
AbelOmarLopez <abellopez1988@gmail.com>
2023-09-22 19:20:31 -0300

    comentarios

--- src/universidad/accesoADatos/AlumnoData.java
+++ src/universidad/accesoADatos/AlumnoData.java
@@ -28,61 +28,61 @@ public class AlumnoData {
     public int guardarAlumno(Alumno alumno) {
         
         String query = "INSERT INTO alumno(dni,apellido, nombre, fechaNacimiento,estado) "
-                + "VALUES (?,?,?,?,?)";
-        int registro = 0;
+                + "VALUES (?,?,?,?,?)";// se define consulta SQL
+        int registro = 0;//  Esta variable se utilizará para almacenar el ID generado del nuevo registro en la base de datos   
         try {
             
-            Alumno al = Vista.getAD().buscarAlumnoPorDni(alumno.getDni());
-            if (al != null) {
+            Alumno al = Vista.getAD().buscarAlumnoPorDni(alumno.getDni());// Esto se hace para verificar si el alumno ya existe en la base de datos
+            if (al != null) {//Se verifica si se encontró un alumno en la base de datos
                 
-                if (al.getId() != alumno.getId()) {
+                if (al.getId() != alumno.getId()) {//compara el ID por parametro con el de la base para no duplicar un DNI
                     
                     return registro;
                 }
             }
 
-            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS); // se preparan los valores de la sentencia SQL junto a las claves generadas.
             ps.setInt(1, alumno.getDni());
             ps.setString(2, alumno.getApellido());
             ps.setString(3, alumno.getNombre());
             ps.setString(4, alumno.getfN().toString());
             ps.setBoolean(5, alumno.isActivo());
-            ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-
-            if (rs.next()) {
+            ps.executeUpdate();// se ejecuta la consulta
+            ResultSet rs = ps.getGeneratedKeys();// se recupera un conjunto de resultados
+            
+            if (rs.next()) {// consulta si rs tiene un valor posterior
                 
-                alumno.setId(rs.getInt(1));
-                registro = rs.getInt(1);
+                alumno.setId(rs.getInt(1));//Si se obtuvieron claves generadas, se asigna el ID generado al objeto alumno
+                registro = rs.getInt(1);// el valor del id se guarda en registro
             } else {                
                 
                 System.out.println("No se pudo recuperar el ID");
             }
-            ps.close();
-        } catch (SQLException e) {
+            ps.close();// se cierra la consulta
+        } catch (SQLException e) {// se captura una posible excepcion SQL
             
             System.out.println("Error al guardar al alumno: " + alumno.getNombre() + " " + alumno.getApellido() + " " + e.getMessage());
         }
-        return registro;
+        return registro;//retorna un registro
     }
     
     //ELIMINAR ALUMNO (DAR DE BAJA)
     public int eliminarAlumnoLogico(int dni) {
         
         String query = "UPDATE alumno SET estado=0 WHERE dni=?";
-        int registro = 0;
+        int registro = 0;//para determinar el cambio en el registro
         try {
             
-            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);//se crea y se prepara la sentencia PS
             ps.setInt(1, dni);
-            registro = ps.executeUpdate();
+            registro = ps.executeUpdate();// Se ejecuta la consulta preparada utilizando el metodo executeUpdate() y se recupera llave generada automaticamente
 
-            ps.close();
+            ps.close();// cierra consulta
         } catch (SQLException e) {
             
             System.out.println("Error al borrar al alumno" + e.getMessage());
         }
-        return registro;
+        return registro; // retorna variable registro al metodo que lo invoca
     }
     
     //EDITAR ALUMNO
@@ -92,23 +92,23 @@ public class AlumnoData {
         int registro = 0;
         try {
             
-            Alumno al = Vista.getAD().buscarAlumnoPorDni(alumno.getDni());
-            if (al != null) {
+            Alumno al = Vista.getAD().buscarAlumnoPorDni(alumno.getDni());//busqueda de alumno por dni en BD utilizando el metodo buscarAlumnoPorDni
+            if (al != null) {//si no encuentra un alumno con el dni no ingresa
                 
-                if (al.getId() != alumno.getId()) {
+                if (al.getId() != alumno.getId()) {// compara el ID pasado por parametro con el de la Base de datos y evita que se repita un DNI
                     
                     return registro;
                 }
             }
             
-            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);//Preparación y ejecución de la consulta SQL
             ps.setInt(1, alumno.getDni());
             ps.setString(2, alumno.getApellido());
             ps.setString(3, alumno.getNombre());
             ps.setString(4, alumno.getfN().toString());
             ps.setInt(5, alumno.getId());
-            registro = ps.executeUpdate();
-            ps.close();
+            registro = ps.executeUpdate(); //ejecuta Update
+            ps.close();//Cierra consulta
         } catch (SQLException e) {
             
             System.out.println("Error editar al alumno : " + alumno.getNombre() + " " + alumno.getApellido() + " " + e.getMessage());
@@ -119,14 +119,14 @@ public class AlumnoData {
     //  BUSCAR ALUMNO POR ID
     public Alumno buscarAlumnoPorId(int id) {
         
-        Alumno al = null;
+        Alumno al = null;//Declara una variable al de tipo Alumno e inicializa su valor como null. Esta variable se utilizará para almacenar el resultado de la búsqueda.
         String query = "SELECT * FROM alumno WHERE idAlumno=? and estado=1";
         try {
             
-            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
+            PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);//se prepara la consulta PS y se solicita las claves generadas automaticamente
+            ps.setInt(1, id);// se modifica el ID
+            ResultSet rs = ps.executeQuery();//se ejecuta la consulta y se almacena en un resulset "rs"
+            if (rs.next()) {// si rs contiene valores, se recuperan abajo
                 
                 al = new Alumno();
                 al.setId(rs.getInt("idAlumno"));
@@ -137,26 +137,26 @@ public class AlumnoData {
                 al.setActivo(rs.getBoolean("estado"));
             } else {
                 
-                JOptionPane.showMessageDialog(null, "No existe el alumno");
+                JOptionPane.showMessageDialog(null, "No existe el alumno");//si no se encuentran alumnos con los parametros buscados deja el msj
             }
-            ps.close();
+            ps.close();//Cierra consulta
         } catch (SQLException e) {
             
             System.out.println("Error al buscar el alumno" + e.getMessage());
         }
-        return al;
+        return al;//retorna un alumno
     }
 
     //  BUSCAR ALUMNO POR DNI
     public Alumno buscarAlumnoPorDni(int dni) {
         
-        Alumno al = null;
-        String query = "SELECT * FROM alumno WHERE dni=?"; //and estado=1";
+        Alumno al = null;// se crea instancia de alumno para utilizarlo dentro del try
+        String query = "SELECT * FROM alumno WHERE dni=?"; 
         try {
             
             PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, dni);
-            ResultSet rs = ps.executeQuery();
+            ps.setInt(1, dni);//se modifica el comodin por el pasado por teclado
+            ResultSet rs = ps.executeQuery();//s
 
             if (rs.next()) {
                 al = new Alumno();
